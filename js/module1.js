b60276c9cf0caa7cda16e85da612579d6ce03f86
lomakina0612 <lomakina0612@gmail.com>
2022-07-09 18:19:56 +0200

    module1 tasks 6-11

--- js/module1.js
+++ js/module1.js
@@ -98,45 +98,170 @@
 
 
 //--TASK 6-------------------------------------------------------
+// Магазин по продаже ремонтных дроидов готов к открытию, осталось написать скрипт для их заказа. Объяви переменные и присвой им соответствующие значения:
 
+// pricePerDroid - цена одного дроида, значение 800
+// orderedQuantity - количество дроидов в заказе, значение 6
+// deliveryFee - стоимость доставки, значение 50
+// totalPrice - общая сумма заказа к оплате, не забудь о стоимости доставки
+// message - сообщение о состоянии заказа в формате "You ordered droids worth <total price> credits. Delivery (<delivery fee> credits) is included in total price."
+
+// // Change code below this line
 
 //--SOLUTION 6---------------------------------------------------
-
+// // Change code below this line
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
 
 
 //--TASK 7-------------------------------------------------------
+// Объяви функцию sayHi, внутри которой добавь console.log() со строкой "Hello, this is my first function!".
+// После объявления вызови функцию sayHi.
 
+// // Change code below this line
 
 //--SOLUTION 7---------------------------------------------------
+// // Change code below this line
+// function sayHi() {
+//  console.log("Hello, this is my first function!")   
+// }
 
+// sayHi();
 
 
 //--TASK 8-------------------------------------------------------
+// Функция add должна уметь складывать три числа и выводить результат в консоль. 
+// Добавь функции add три параметра a, b и c, которые будут получать значения аргументов при её вызове.
+
+// Дополни console.log() так, чтобы он логировал строку "Addition result equals <result>", где < result > это сумма переданных чисел.
+
+// // Change code below this line
+// function add() {
+//   console.log();
+//   // Change code above this line
+// }
+
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
 
 
 //--SOLUTION 8---------------------------------------------------
+// // Change code below this line
+// function add(a,b,c) {
+//   let result = a + b + c;
+//   console.log(`Addition result equals ${result}`);
+//   // Change code above this line
+// }
 
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
 
 
 //--TASK 9-------------------------------------------------------
+// Дополни код функции add так, чтобы она возвращала результат сложения значений трёх параметров a, b и c.
 
+// function add(a, b, c) {
+//   // Change code below this line
+
+
+
+//   // Change code above this line
+// }
+
+// add(2, 5, 8); // 15
+
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
 
 //--SOLUTION 9---------------------------------------------------
+// function add(a, b, c) {
+//   // Change code below this line
 
+//   return a + b + c;
+
+//   // Change code above this line
+// }
+
+// add(2, 5, 8); // 15
+
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
 
 
 //--TASK 10-------------------------------------------------------
+// Функция makeMessage(name, price) составляет и возвращает сообщение о покупке. 
+// Она объявляет два параметра, значения которых будут задаваться во время её вызова.
 
+//     name - название товара
+//     price - цена товара
+// Дополни код функции так, чтобы в переменную message записывалась строка
+// "You picked <product name>, price per item is <product price> credits", где < product name > и < product price >
+// это значения параметров name и price.Используй синтаксис шаблонных строк.
+
+// function makeMessage (name, price) {
+//   // Change code below this line
+//    const message = `You picked ${name}, price per item is ${price} credits`;
+//   // Change code above this line
+//   return message;
+// };
+
+// makeMessage ('Radar', 6150);
+// makeMessage('Scanner', 3500);
+// makeMessage('Reactor', 8000);
+// makeMessage('Engine', 4070);
 
 //--SOLUTION 10---------------------------------------------------
+// function makeMessage (name, price) {
+//   // Change code below this line
+//    const message = `You picked ${name}, price per item is ${price} credits`;
+//   // Change code above this line
+//   return message;
+// };
 
+// makeMessage ('Radar', 6150);
+// makeMessage('Scanner', 3500);
+// makeMessage('Reactor', 8000);
+// makeMessage('Engine', 4070);
 
 
 //--TASK 11-------------------------------------------------------
+// Функция calculateTotalPrice считает и возвращает общую сумму покупки. 
+// Она принимает два параметра, значения которых будут задаваться во время её вызова.
 
+//     orderedQuantity - количество единиц товара в заказе;
+//     pricePerItem - цена одной единицы товара.
+// Дополни код функции так, чтобы в переменную totalPrice записывалась общая сумма покупки,
+// результат умножения кол - ва товаров на цену одного.
+
+// function calculateTotalPrice (orderedQuantity, pricePerItem) {
+//   // Change code below this line
+//   const totalPrice = ;
+
+//   // Change code above this line
+//   return totalPrice;
+// };
 
 //--SOLUTION 11---------------------------------------------------
+function calculateTotalPrice (orderedQuantity, pricePerItem) {
+  // Change code below this line
+  const totalPrice = orderedQuantity * pricePerItem;
 
+  // Change code above this line
+  return totalPrice;
+};
+
+calculateTotalPrice(5, 100);
+calculateTotalPrice(8, 60);
+calculateTotalPrice(3, 400);
+calculateTotalPrice(1, 3500);
+calculateTotalPrice(12, 70);
 
 
 
