bf02d6586bd71a10bbd18be862b47720befba41c
lomakina0612 <lomakina0612@gmail.com>
2022-07-17 00:38:10 +0200

    module4 tasks 17-24

--- js/module4.js
+++ js/module4.js
@@ -552,39 +552,270 @@
 
 
 //--TASK 17--------------------------------------------------------------------------------------------------------------
+// Этот массив объектов мы будем передавать в параметр users при вызове функции 
+// из задания.
 
+const users = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    skills: ["ipsum", "lorem"],
+    gender: "male",
+    age: 37,
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+    gender: "female",
+    age: 34,
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+    gender: "male",
+    age: 24,
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    skills: ["adipisicing", "irure", "velit"],
+    gender: "female",
+    age: 21,
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    skills: ["ex", "culpa", "nostrud"],
+    gender: "male",
+    age: 27,
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    skills: ["non", "amet", "ipsum"],
+    gender: "male",
+    age: 38,
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    skills: ["lorem", "veniam", "culpa"],
+    gender: "female",
+    age: 39,
+  },
+];
+
+// Дополни функцию getUserNames(users) так, чтобы она возвращала массив имён
+// пользователей(свойство name) из массива объектов в параметре users.
+
+// // Change code below this line
+// const getUserNames = users => {
+    
+
+// };
+// // Change code above this line
 
 //--SOLUTION 17---------------------------------------------------
+// // Change code below this line
+// const getUserNames = users => {
+//     return users.map(user => user.name)
 
+// };
+// // Change code above this line
+
+// console.log(getUserNames(users));
 
 
 //--TASK 18--------------------------------------------------------------------------------------------------------------
+// Дополни функцию getUserEmails(users) так, чтобы она возвращала массив
+// почтовых адресов пользователей(свойство email) из массива объектов в
+// параметре users.
 
+// // Change code below this line
+// const getUserEmails = users => {
+    
+
+// };
+// // Change code above this line
 
 //--SOLUTION 18---------------------------------------------------
+// // Change code below this line
+// const getUserEmails = users => {
+//   return users.map(user => user.email);
+// };
+// // Change code above this line
 
+// console.log(getUserEmails(users));
 
 
 //--TASK 19--------------------------------------------------------------------------------------------------------------
+// Дополни код так, чтобы в переменной evenNumbers получился массив чётных
+// чисел из массива numbers, а в переменной oddNumbers массив нечётных.
+// Обязательно используй метод filter().
 
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// // Change code below this line
+
+// const evenNumbers = numbers;
+// const oddNumbers = numbers;
 
 //--SOLUTION 19---------------------------------------------------
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// // Change code below this line
 
+// const evenNumbers = numbers.filter(number => number % 2 === 0);
+// const oddNumbers = numbers.filter(number => number % 2 !== 0);
+
+// console.log(evenNumbers);
+// console.log(oddNumbers);
 
 
 //--TASK 20--------------------------------------------------------------------------------------------------------------
+// Дополни код так, чтобы в переменной allGenres был массив всех жанров
+// книг(свойство genres) из массива books, а в переменной uniqueGenres массив
+// уникальных жанров - без повторений.
 
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["adventure", "history"],
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     genres: ["fiction", "mysticism"],
+//   },
+//   {
+//     title: "Redder Than Blood",
+//     author: "Tanith Lee",
+//     genres: ["horror", "mysticism", "adventure"],
+//   },
+// ];
+// // Change code below this line
+// const allGenres = books;
+// const uniqueGenres = allGenres;
 
 //--SOLUTION 20---------------------------------------------------
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["adventure", "history"],
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     genres: ["fiction", "mysticism"],
+//   },
+//   {
+//     title: "Redder Than Blood",
+//     author: "Tanith Lee",
+//     genres: ["horror", "mysticism", "adventure"],
+//   },
+// ];
+// // Change code below this line
+// const allGenres = books.flatMap(book => book.genres);
+// const uniqueGenres = allGenres.filter(
+//     (genre, index, arr) => arr.indexOf(genre) === index
+//   );
 
+// console.log(allGenres);
+// console.log(uniqueGenres);
 
 
 //--TASK 21--------------------------------------------------------------------------------------------------------------
+// Используя метод filter() дополни код так, чтобы:
 
+// В переменной topRatedBooks получился массив книг рейтинг которых
+//   (свойство rating) больше либо равно значению переменной MIN_RATING.
+// В переменной booksByAuthor получился массив книг написанных автором 
+// с именем(свойство author) которое совпадает со значением в переменной AUTHOR.
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+
+// const MIN_RATING = 8;
+// const AUTHOR = "Bernard Cornwell";
+// // Change code below this line
+
+// const topRatedBooks = books;
+// const booksByAuthor = books;
 
 //--SOLUTION 21---------------------------------------------------
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
 
+// const MIN_RATING = 8;
+// const AUTHOR = "Bernard Cornwell";
+// // Change code below this line
 
+// const topRatedBooks = books.filter(book => book.rating >= MIN_RATING);
+// const booksByAuthor = books.filter (book => book.author === AUTHOR);
 
 
 //--TASK 22--------------------------------------------------------------------------------------------------------------
