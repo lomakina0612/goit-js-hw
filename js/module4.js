ba1302cc6c807e2a0ff3b7e001c7abf0d352a7b5
lomakina0612 <lomakina0612@gmail.com>
2022-07-17 15:47:04 +0200

    module4 tasks 22-25

--- js/module4.js
+++ js/module4.js
@@ -555,85 +555,85 @@
 // Этот массив объектов мы будем передавать в параметр users при вызове функции 
 // из задания.
 
-const users = [
-  {
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    eyeColor: "blue",
-    friends: ["Sharron Pace"],
-    isActive: false,
-    balance: 2811,
-    skills: ["ipsum", "lorem"],
-    gender: "male",
-    age: 37,
-  },
-  {
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    eyeColor: "blue",
-    friends: ["Briana Decker", "Sharron Pace"],
-    isActive: true,
-    balance: 3821,
-    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
-    gender: "female",
-    age: 34,
-  },
-  {
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    eyeColor: "green",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-    isActive: false,
-    balance: 3793,
-    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
-    gender: "male",
-    age: 24,
-  },
-  {
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    eyeColor: "green",
-    friends: ["Goldie Gentry", "Aisha Tran"],
-    isActive: true,
-    balance: 2278,
-    skills: ["adipisicing", "irure", "velit"],
-    gender: "female",
-    age: 21,
-  },
-  {
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    eyeColor: "blue",
-    friends: ["Jordan Sampson", "Eddie Strong"],
-    isActive: true,
-    balance: 3951,
-    skills: ["ex", "culpa", "nostrud"],
-    gender: "male",
-    age: 27,
-  },
-  {
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    eyeColor: "brown",
-    friends: ["Jacklyn Lucas", "Linda Chapman"],
-    isActive: false,
-    balance: 1498,
-    skills: ["non", "amet", "ipsum"],
-    gender: "male",
-    age: 38,
-  },
-  {
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    eyeColor: "brown",
-    friends: ["Goldie Gentry", "Briana Decker"],
-    isActive: true,
-    balance: 2764,
-    skills: ["lorem", "veniam", "culpa"],
-    gender: "female",
-    age: 39,
-  },
-];
+// const users = [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     skills: ["ipsum", "lorem"],
+//     gender: "male",
+//     age: 37,
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+//     gender: "female",
+//     age: 34,
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+//     gender: "male",
+//     age: 24,
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     skills: ["adipisicing", "irure", "velit"],
+//     gender: "female",
+//     age: 21,
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     skills: ["ex", "culpa", "nostrud"],
+//     gender: "male",
+//     age: 27,
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     skills: ["non", "amet", "ipsum"],
+//     gender: "male",
+//     age: 38,
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     skills: ["lorem", "veniam", "culpa"],
+//     gender: "female",
+//     age: 39,
+//   },
+// ];
 
 // Дополни функцию getUserNames(users) так, чтобы она возвращала массив имён
 // пользователей(свойство name) из массива объектов в параметре users.
@@ -759,7 +759,7 @@ const users = [
 
 // В переменной topRatedBooks получился массив книг рейтинг которых
 //   (свойство rating) больше либо равно значению переменной MIN_RATING.
-// В переменной booksByAuthor получился массив книг написанных автором 
+// В переменной booksByAuthor получился массив книг написанных автором
 // с именем(свойство author) которое совпадает со значением в переменной AUTHOR.
 
 // const books = [
@@ -819,32 +819,239 @@ const users = [
 
 
 //--TASK 22--------------------------------------------------------------------------------------------------------------
+// Этот массив объектов мы будем передавать в параметр users при вызове
+// функции из задания.
 
+// users = [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     gender: "male"
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     gender: "female"
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     gender: "male"
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     gender: "female"
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     gender: "male"
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     gender: "male"
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     gender: "female"
+//   }
+// ]
+
+// Дополни функцию getUsersWithEyeColor(users, color) так, чтобы она возвращала
+// массив пользователей у которых цвет глаз(свойство eyeColor) совпадает со
+// значением параметра color.
+
+// // Change code below this line
+// const getUsersWithEyeColor = (users, color) => {
+
+// };
+// // Change code above this line
 
 //--SOLUTION 22---------------------------------------------------
-
+ const users = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male",
+    age: 37
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female",
+    age: 34
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male",
+    age: 24
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female",
+    age: 21
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male",
+    age: 27
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male",
+    age: 38
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female",
+    age: 39
+  }
+]
+// // Change code below this line
+// const getUsersWithEyeColor = (users, color) => {
+//   return users.filter(user => user.eyeColor === color)
+// };
+// // Change code above this line
 
 
 //--TASK 23--------------------------------------------------------------------------------------------------------------
+// Дополни функцию getUsersWithAge(users, minAge, maxAge) так, чтобы она
+// возвращала массив пользователей, возраст которых(свойство age) попадает в
+// промежуток от minAge до maxAge.
 
+// // Change code below this line
+// const getUsersWithAge = (users, minAge, maxAge) => {
+ 
+
+// };
+// // Change code above this line
 
 //--SOLUTION 23---------------------------------------------------
+// // Change code below this line
+// const getUsersWithAge = (users, minAge, maxAge) => {
+//   return users.filter(
+//     ({ age }) => {
+//       return minAge <= age && age <= maxAge;
+//     }
+//   )
+// };
+// // Change code above this line
 
+// console.log(getUsersWithAge(users, 20, 30));
 
 
 //--TASK 24--------------------------------------------------------------------------------------------------------------
+// Дополни функцию getUsersWithFriend(users, friendName) так, чтобы она
+// возвращала массив пользователей у которых есть друг с именем в параметре
+// friendName.Массив друзей пользователя хранится в свойстве friends.
 
+// // Change code below this line
+// const getUsersWithFriend = (users, friendName) => {
+   
+// };
+// // Change code above this line
 
 //--SOLUTION 24---------------------------------------------------
+// // Change code below this line
+// const getUsersWithFriend = (users, friendName) => {
+//   return users.filter(
+//      user => user.friends.some(friend => friend === friendName)
+//    )
+// };
+// // Change code above this line
 
-
+// console.log(getUsersWithFriend(users, "Briana Decker"));
 
 
 //--TASK 25--------------------------------------------------------------------------------------------------------------
+// Дополни функцию getFriends(users) так, чтобы она возвращала массив друзей 
+// всех пользователей(свойство friends). У нескольких пользователей могут быть 
+// одинаковые друзья, сделай так чтобы возвращаемый массив не содержал повторений.
 
+// // Change code below this line
+// const getFriends = (users) => {
+   
+// };
+// // Change code above this line
 
 //--SOLUTION 25---------------------------------------------------
+// // Change code below this line
+// const getFriends = (users) => {
+//    const allFriends = users.flatMap(
+//     user => user.friends
+//   )
 
+//   return allFriends.filter(
+//     (friend, index, arr) => arr.indexOf(friend) === index
+//   )
+// };
+// // Change code above this line
+
+// console.log(getFriends(users));
 
 
 //--TASK 26--------------------------------------------------------------------------------------------------------------
