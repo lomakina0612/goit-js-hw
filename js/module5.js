6629f5f0fa05b801541818d03a9568da27184c03
Elena Lomakina <lomakina0612@gmail.com>
2022-07-18 22:02:07 +0200

    module 5 task 3

--- js/module5.js
+++ js/module5.js
@@ -114,12 +114,66 @@
 // console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
 
 
-
 //--TASK 3--------------------------------------------------------------------------------------------------------------
+// Тестировщики нашли баги в коде сервиса хранения истории заказов еды.
+// Тебе необходимо исправить их, правильно расставив this в методах объекта
+// historyService, чтобы методы начали работать правильно.
 
+// const historyService = {
+//   orders: [
+//     { email: "jacob@hotmail.com", dish: "Burrito" },
+//     { email: "solomon@topmail.net", dish: "Burger" },
+//     { email: "artemis@coldmail.net", dish: "Pizza" },
+//     { email: "solomon@topmail.net", dish: "Apple pie" },
+//     { email: "jacob@hotmail.com", dish: "Taco" },
+//   ],
+//   // Change code below this line
+//   getOrdersLog() {
+//     return orders
+//       .map(order => `email: ${order.email} dish: ${order.dish}`)
+//       .join(" - ");
+//   },
+//   getEmails() {
+//     const emails = orders.map(order => order.email);
+//     const uniqueEmails = new Set(emails);
+//     return [...uniqueEmails];
+//   },
+//   getOrdersByEmail(email) {
+//     return orders.filter(order => order.email === email);
+//   },
+//   // Change code above this line
+// };
 
 //--SOLUTION 3---------------------------------------------------
+// const historyService = {
+//   orders: [
+//     { email: "jacob@hotmail.com", dish: "Burrito" },
+//     { email: "solomon@topmail.net", dish: "Burger" },
+//     { email: "artemis@coldmail.net", dish: "Pizza" },
+//     { email: "solomon@topmail.net", dish: "Apple pie" },
+//     { email: "jacob@hotmail.com", dish: "Taco" },
+//   ],
+//   // Change code below this line
+//   getOrdersLog() {
+//     return this.orders
+//       .map(order => `email: ${order.email} dish: ${order.dish}`)
+//       .join(" - ");
+//   },
+//   getEmails() {
+//     const emails = this.orders.map(order => order.email);
+//     const uniqueEmails = new Set(emails);
+//     return [...uniqueEmails];
+//   },
+//   getOrdersByEmail(email) {
+//     return this.orders.filter(order => order.email === email);
+//   },
+//   // Change code above this line
+// };
 
+// console.log(historyService);
+// console.log(historyService.getEmails());
+// console.log(historyService.getOrdersByEmail("jacob@hotmail.com"));
+// console.log(historyService.getOrdersLog());
 
 
 //--TASK 4--------------------------------------------------------------------------------------------------------------
